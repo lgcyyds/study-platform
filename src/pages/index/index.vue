221b2e80e1513e3a4c36f868ed158266434ac3eb
lgc <2284483745@qq.com>
2023-12-15 13:06:56 +0800

    新增页面，首页轮播图

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,48 +1,39 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
+	<view class="content">
+		<!-- 轮播图 -->
+		<view class="banner">
+			<swiper indicator-dots indicator-active-color="#fff" autoplay circular interval="5000">
+				<swiper-item v-for="(item, index) in banner" :key="index">
+					<image class="banner-img" :src="item.url" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<!-- 点击签到按钮 -->
+		
+	</view>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      title: 'Hello',
-    }
-  },
-  onLoad() {},
-  methods: {},
-}
+<script setup>
+import { onLoad } from '@dcloudio/uni-app';
+import { ref } from 'vue';
+let banner = ref([
+	{
+		url: 'https://img1.baidu.com/it/u=507850114,3105556430&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
+	},
+	{
+		url: 'https://img0.baidu.com/it/u=1032314795,3433105171&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500'
+	}
+]);
+onLoad(() => {});
 </script>
 
-<style>
+<style lang="less">
 .content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+	.banner {
+		.banner-img {
+			width: 100%;
+			height: 100%;
+		}
+	}
 }
 </style>
