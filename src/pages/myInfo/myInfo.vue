0c5e9d8383cab5bd9174abffd247d3c525577299
lgc <2284483745@qq.com>
2024-01-04 15:36:04 +0800

    新增头像上传

--- src/pages/myInfo/myInfo.vue
+++ src/pages/myInfo/myInfo.vue
@@ -1,7 +1,20 @@
 <template>
 	<view class="info_container">
 		<view class="info_image">
-			<van-uploader :file-list="fileList" @after-read="afterRead" preview-size="180rpx" accept="image" max-count="1"/>
+			<uni-file-picker 
+				class="imageUploader"
+				v-model="imageValue" 
+				fileMediatype="image" 
+				:limit="1"
+				:del-icon="false"
+				disable-preview
+				mode:grid
+				:image-styles="imageStyle"
+				@select="select" 
+				@progress="progress" 
+				@success="success" 
+				@fail="fail" 
+			/>
 		</view>
 		<view class="info_msg">
 			
@@ -11,14 +24,33 @@
 
 <script setup>
 import { ref } from 'vue';
-let fileList = ref([])
-let afterRead = (event)=>{
-	console.log(event.detail.file);
-	fileList.value.push({
-		url:event.detail.file.url
-	})
-	console.log(fileList.value);
+const imageValue = ref([])
+const imageStyle = ref({
+	"height": 100,	// 边框高度
+	"width": 100,	// 边框宽度
+	"border":{ // 如果为 Boolean 值，可以控制边框显示与否
+		"radius":"50%" 		// 边框圆角，支持百分比
+	}
+})
+// 获取上传状态
+const select = (e)=>{
+	console.log('选择文件：',e)
 }
+// // 获取上传进度
+const progress = (e)=>{
+	console.log('上传进度：',e)
+}
+
+// // 上传成功
+const success = (e)=>{
+	console.log('上传成功')
+}
+
+// // 上传失败
+const fail = (e)=>{
+	console.log('上传失败：',e)
+}
+
 </script>
 
 <style lang="less" scoped>
@@ -31,8 +63,11 @@ let afterRead = (event)=>{
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		/deep/.van-uploader__upload{
-			border-radius: 50%;
+		/deep/.uni-file-picker {
+			    overflow: visible;
+			    flex: 1;
+			    width: 100rpx;
+			    height: 100rpx;
 		}
 	}
 }
