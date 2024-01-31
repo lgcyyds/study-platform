295ca9002ec9fa72634722d82e5ae7a87cdb57ff
LGC <2284483745@qq.com>
2024-01-31 21:46:22 +0800

    新增pinia

--- src/main.js
+++ src/main.js
@@ -2,17 +2,13 @@ import {
 	createSSRApp
 } from "vue";
 import App from "./App.vue";
-// import { Tab, Tabs ,Icon,Button,Tag,DropdownMenu, DropdownItem } from 'vant';
-// import 'vant/lib/index.css';
+import {
+	createPinia
+} from 'pinia'
+const pinia = createPinia()
 export function createApp() {
 	const app = createSSRApp(App);
-	// app.use(Tab);
-	// app.use(Tabs);
-	// app.use(Icon);
-	// app.use(Button);
-	// app.use(Tag );
-	// app.use(DropdownMenu);
-	// app.use(DropdownItem);
+	app.use(pinia);
 	return {
 		app,
 	};
